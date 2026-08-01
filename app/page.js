5eae20c644243d94b3678d50758df289a7ce30ba
ishuklaji <ishuklaji100@gmail.com>
2026-08-02 01:01:46 +0200

    project initiated

---
+++ app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
+      <h1 className="text-2xl font-bold">Hello, Next.js!</h1>
+    </div>
+  );
+}
