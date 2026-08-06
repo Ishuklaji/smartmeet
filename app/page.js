b967c9bda82f44af37f264b4729dc55e6a6c304b
ishuklaji <ishuklaji100@gmail.com>
2026-08-06 22:31:04 +0200

    m

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
       <div className="p-8 bg-gray-800/60 rounded-2xl border border-gray-700 w-80 backdrop-blur-sm shadow-2xl">
         <input
           className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray-600 text-white placeholder-gray-400 focus:ring-2 focus:ring-blue-500 outline-none transition"
